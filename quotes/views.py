0786deb34b2dc21749efd9a387adfbc2bbab7381
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-05-11 14:36:11 +0800

    add checking stock ticker valid or not logic

--- quotes/views.py
+++ quotes/views.py
@@ -22,7 +22,29 @@ def search_stock(base_url, stock_ticker):
     except Exception as e:
         data = {'Error':'There has been some connection error. Please try again later.'}
     return data
-   
+
+def search_stock_batch(base_url, stock_tickers):
+    try:
+        token = settings.IEXCLOUD_TEST_API_TOKEN
+        url = base_url + stock_tickers + '&types=quote&token=' + token
+        print(f'url: {url}')
+        data = requests.get(url)
+
+        if data.status_code == 200:
+            data = json.loads(data.content)
+        else:
+            data = {'Error' : 'There has been an unexpected issues. Please try again'}
+    except Exception as e:
+        data = {'Error':'There has been some connection error. Please try again later.'}
+    return data
+
+def check_valid_stock_ticker(ticker):
+    base_url = 'https://sandbox.iexapis.com/stable/stock/'
+    stock = search_stock(base_url, ticker)
+    if 'Error' not in stock:
+        return True
+    return False
+
 def home(request):
     if request.method == 'POST':
         stock_ticker = request.POST['stock_ticker']
@@ -36,29 +58,40 @@ def about(request):
 
 def portfolio(request):
     if request.method == 'POST':
-        if request.POST['ticker']:
+        ticker = request.POST['ticker']
+        if ticker:
             form = StockForm(request.POST or None)
-
             if form.is_valid():
-                form.save()
-                messages.success(request, 'stock has been added successfully.')
-        else:
-            messages.warning(request, 'Please enter ticker name.')
+                if check_valid_stock_ticker(ticker):
+                    form.save()
+                    messages.success(request, f'{ticker} has been added successfully.')
+                    return redirect('portfolio')
+
+        messages.warning(request, 'Please enter a valid ticker name.')
         return redirect('portfolio')
     else:
         stocks = Stock.objects.all()
-        return render(request, 'quotes/portfolio.html', {'stocks':stocks})
+        if stocks:
+            ticker_list = [stock.ticker for stock in stocks]
+            ticker_list = list(set(ticker_list))
+            
+            tickers = ','.join(ticker_list)
+            base_url = 'https://sandbox.iexapis.com/stable/stock/market/batch?symbols='
+            data = search_stock_batch(base_url, tickers)
+            print(data)
+
+        return render(request, 'quotes/portfolio.html', {'stocks':stocks, 'data':data})
 
 def delete_stock(request, stock_id):
     stock = Stock.objects.get(pk=stock_id)
     stock.delete()
 
-    messages.success(request, 'stock has been deleted successfully.')
+    messages.success(request, f'{stock.ticker} has been deleted successfully.')
     return redirect('portfolio')
 
 
 if __name__ == "__main__":
-    base_url = 'https://sandbox.iexapis.com/stable/stock/'
-    stock_ticker = 'IBM'
-
-    data = connect(base_url, stock_ticker)
+    pass
+    # base_url = 'https://sandbox.iexapis.com/stable/stock/'
+    # stock_ticker = 'IBM'
+    # data = search_stock(base_url, stock_ticker)
