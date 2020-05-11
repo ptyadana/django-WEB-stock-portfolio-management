66d154a3a8df6c090e3c2249564d610dd778eb96
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-05-11 18:35:48 +0800

    add logic for converting Upper letter for ticker upon saving

--- quotes/models.py
+++ quotes/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Stock(models.Model):
     ticker = models.CharField(max_length=10)
 
+    def clean(self):
+        self.ticker = self.ticker.upper()
+
     def __str__(self):
         return self.ticker
     
